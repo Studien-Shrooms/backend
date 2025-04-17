e0f2c2f84047a78c69d19e944205e6fc77879023
JESS-L-ROBOT <zickgrafj@gmail.com>
2025-04-17 19:02:42 +0200

    Anfragen von FrontendURL erlauben

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],  
+    allow_origins=["http://localhost:4200", 
+                   "https://thankful-desert-06810b503.6.azurestaticapps.net/"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
