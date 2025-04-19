16364994d14dc308e3cf10670ccc675abfb7e9e4
JESS-L-ROBOT <zickgrafj@gmail.com>
2025-04-19 18:39:18 +0200

    allow_origins geupdatet

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:4200", 
+                   "https://thankful-desert-06810b503.6.azurestaticapps.net",
                    "https://thankful-desert-06810b503.6.azurestaticapps.net/"],  
     allow_credentials=True,
     allow_methods=["*"],
